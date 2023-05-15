e2d044e791f5eaeb9e4eb964514a7f8a855f38c0
Oleksandr Shakhov <86519684+OleksandrShakhov@users.noreply.github.com>
2023-05-14 20:05:44 -0600

    Update Contact.jsx

--- src/Components/Contact Section/Contact.jsx
+++ src/Components/Contact Section/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './contact.css';
 // Importend icons
 import {BsInstagram} from 'react-icons/bs'
@@ -6,21 +6,41 @@ import {BsArrowRightSquare} from 'react-icons/bs'
 import {BsTelegram} from 'react-icons/bs'
 import {FaFacebookMessenger} from 'react-icons/fa'
 import {CgArrowRightR} from 'react-icons/cg'
+import {GrFormClose} from 'react-icons/gr'
+import {MdOutlineMarkEmailRead} from 'react-icons/md'
 
 //EmailJS 
-import emailjs from '@emailjs/browser';
-
-
-
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
-	const form = useRef();
+  const [isSent, setIsSent] = useState(false); // initialize state to false
+
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        'shakhov42@gmail.com',
+        'template_ehwqx9l',
+        e.target,
+        '8AbWo945hxovHGhuz'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsSent(true); // set state to true when email is sent successfully
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+	/*const form = useRef();
 	const sendEmail = (e) => {
 		e.preventDefault();
 		
 		emailjs.sendForm('shakhov42@gmail.com', 'template_ehwqx9l', form.current, '8AbWo945hxovHGhuz')
-		e.target.reset() // to reset the form after submit
+		e.target.reset() // to reset the form after submit*/
 	};
 
 	return (
@@ -95,7 +115,7 @@ export default function Contact() {
 				<div className="form grid">
 					<h3>Send me an Email</h3>
 
-					<form ref={form} onSubmit={sendEmail}>
+					<form /*ref={form}*/ onSubmit={sendEmail} >
 						<input type="text" placeholder='Enter your Name' name='name'  />
 						<input type="email" placeholder='Enter your Email' name='email' />
 						<textarea name="message" placeholder='Enter your Message'></textarea>
@@ -103,6 +123,14 @@ export default function Contact() {
 							Send Email
 						</button>
 					</form>
+          {isSent && (
+            <div className="popup">
+              <MdOutlineMarkEmailRead className='icon-email'/>
+              <p className='popup-text-1'>Thanks for your email</p>
+              <p className='popup-text-2'>Your message has been successfully sent!</p>
+              <button className='btn-popup' onClick={() => setIsSent(false)}><GrFormClose className='icon-close' /></button>
+            </div>
+          )}
 				</div>
 			</div>
 			<div className="forGoUp">
